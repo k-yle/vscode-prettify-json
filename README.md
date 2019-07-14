33ab68100eca2a77e33c9ee428573b6f6505703a
Kyle Hensel <k-yle@users.noreply.github.com>
2019-07-14 12:52:13 +1200

    :shipit: v1

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
-# Visual Studio Code Prettify JSON Extension
-
-> Prettify ugly JSON inside VSCode
-
-**[Prettify JSON in Visual Studio Marketplace](https://marketplace.visualstudio.com/items/mohsen1.prettify-json)**
-
-![Screenshot](https://raw.githubusercontent.com/mohsen1/vscode-prettify-json/master/screenshot.gif)
-
-## Installation
-
-Install via package manager. Hit `F1` or `"cmd+shift+p"` and type `install` and then type `Prettify JSON` and hit enter.
-
-## License
-MIT
+# VS Code JSON Prettier (Compact version)
+This is a fork of [Prettify JSON](https://marketplace.visualstudio.com/items/mohsen1.prettify-json), which uses [json-stringify-pretty-compact](https://npm.im/json-stringify-pretty-compact) to format JSON. 
--- src/extension.ts
+++ src/extension.ts
@@ -2,62 +2,56 @@ import {
   ExtensionContext,
   Position,
   Range,
-  TextEditorDecorationType,
   window,
   commands,
-  workspace
+  workspace,
 } from 'vscode';
 
-import * as vscode from 'vscode';
-
-const stripComments = require('strip-json-comments');
-const jsonlint = require('jsonlint');
+import * as stripComments from 'strip-json-comments';
+import * as prettyCompact from 'json-stringify-pretty-compact';
+import * as jsonlint from 'jsonlint';
 
 const LINE_SEPERATOR = /\n|\r\n/;
 
 // TODO: make this configurable.
 const JSON_SPACE = 4;
 
-export function activate(context: ExtensionContext) {
-
-  let disposable = commands.registerCommand('extension.prettifyJSON', () => {
-
+export function activate(context: ExtensionContext): void {
+  const disposable = commands.registerCommand('extension.prettifyCompact', (): void => {
     const editor = window.activeTextEditor;
 
-    if (!editor) {
-      return;
-    }
+    if (!editor) return;
 
-    const editor_config = workspace.getConfiguration('editor');
-    const tab_size = editor_config.get('tabSize', JSON_SPACE);
+
+    const editorConfig = workspace.getConfiguration('editor');
+    const tabSize = editorConfig.get('tabSize', JSON_SPACE);
 
     const raw = editor.document.getText();
     let json = null;
 
     try {
       json = jsonlint.parse(stripComments(raw));
-    } catch (jsonLintError) {
-      const message: string = jsonLintError.message;
+    } catch ({ message }) {
       const lineNumber = parseInt(message.substring(message.indexOf('line ') + 5, message.indexOf(':')), 10);
-
+      console.error(`Line ${lineNumber}: ${message}`);
 
       return;
     }
 
-    let pretty = JSON.stringify(json, null, tab_size);
+    const pretty = prettyCompact(json, {
+      indent: tabSize,
+      maxLength: 200,
+    });
 
-    editor.edit(builder=> {
+    editor.edit((builder): void => {
       const start = new Position(0, 0);
       const lines = raw.split(LINE_SEPERATOR);
       const end = new Position(lines.length, lines[lines.length - 1].length);
       const allRange = new Range(start, end);
       builder.replace(allRange, pretty);
-    }).then(success=> {
-
+    }).then((): void => {
       // TODO: unselect the text
-
     });
-
   });
 
   context.subscriptions.push(disposable);
